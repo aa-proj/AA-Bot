1054323ee23f8b71c96072f1c6310f8ab7aa3449
rassi0429 <taitokokoa+rassi@gmail.com>
2023-01-26 11:07:59 +0900

    feat: comment

--- src/apps/neruokirubot/src/index.ts
+++ src/apps/neruokirubot/src/index.ts
@@ -10,7 +10,6 @@ import {getHoursFromMills, getTimeFromMills, giveAAPoint} from "./util";
 import {OGPManager} from "./ogp";
 import {AppBase} from "../../appBase";
 import express from "express";
-import {Furo_Result} from "../../furohaittakabot/src/main";
 
 
 // TypeORMのオプション
@@ -41,6 +40,9 @@ export class NeruOkiruBot extends AppBase {
   // TypeORMのコネクション 使う前にnullチェックが必要
   connection: Connection | null = null;
 
+  // コンストラクタ
+  // アプリがAA-Botに登録されるときに１度だけ呼び出される
+  // ここでデータベースに繋いだり、最初に準備する処理を入れる
   constructor(client: Client) {
     super(client);
     this.appName = "NeruOkiruBot"
@@ -49,17 +51,23 @@ export class NeruOkiruBot extends AppBase {
     this.connectDB();
     this.OGP = new OGPManager();
 
+
+    /** ここから HTTP APIの定義 */
     this.apiRoot = "/sleep"
     this.apiRouter = express.Router()
+
+    // 死活監視
     this.apiRouter.get("/", (req, res) => {
       res.send("neruokiru ok")
     })
 
+    // 寝るデータをとる
     this.apiRouter.get("/user/:id", async (req, res) => {
       const data = await this.getUserNeruData(req.params.id)
       res.json(data)
     })
 
+    // 起きる
     this.apiRouter.post("/user/:id/okiru", async (req, res) => {
       try {
         await this.doOkiru(req.params.id)
@@ -69,7 +77,7 @@ export class NeruOkiruBot extends AppBase {
       }
     })
 
-
+    // 寝る
     this.apiRouter.post("/user/:id/neru", async (req, res) => {
       try {
         await this.doNeru(req.params.id)
@@ -78,14 +86,22 @@ export class NeruOkiruBot extends AppBase {
         res.status(500).json({success: false})
       }
     })
+
+    /** ここまで */
   }
 
+  // Botが準備完了になったとき
   override async onBotReady(arg: Client<true>) {
     this.log("OnBotReady")
   }
 
+  // 普通のテキストメッセージが来たとき
   override async onMessageCreate(msg: Message) {
+    // Botの時はやらない
     if (msg.author.bot) return;
+
+    // コマンド "/no" から始まってない時
+    // CMD_PREFIX環境変数を持っている時は、/${CMD_PREFIX}no
     let len = 3
     if(process.env.CMD_PREFIX) {
       if (!msg.content.startsWith(`/${process.env.CMD_PREFIX}no`)) {
@@ -96,8 +112,13 @@ export class NeruOkiruBot extends AppBase {
     else if (!msg.content.startsWith("/no")) {
       return;
     }
+
     // 引数をパース
+    // argsに /no 以降のコマンドを配列に入れる
+    // /no init　なら argsには ["init"]が入る
     const args = msg.content.replace(/　+/g, " ").slice(len).split(/ +/) //.trim().split(/ /);
+
+    // args[0] によって処理をかえる
     switch (args[0]) {
       case "init":
         await this.initBotMessage();
@@ -120,16 +141,23 @@ export class NeruOkiruBot extends AppBase {
     }
   }
 
+  // ボタンとかコマンドとかのインタラクションが来たとき
   override async onInteractionCreate(interaction: Interaction<CacheType>) {
 
+    // ボタンを押された時ではない
+    // ああサーバからのアクションではない
+    // 時は処理しない
     if (!interaction.isButton() || !interaction.guildId) {
       return;
     }
 
+    // Botがおした時は処理しない
     if (interaction.user.bot) return;
 
+    // コマンドを取り出し コマンドは initBotMessageメソッドのsetCustomIdで定義
     const command = interaction.customId;
 
+    // コマンドで分岐
     switch (command) {
       case "Neru":
         await this.doNeru(interaction.user.id)
@@ -142,16 +170,24 @@ export class NeruOkiruBot extends AppBase {
     }
   }
 
+  // 寝る処理メソッド
   async doNeru(userId: string) {
+    // いろいろとってくる
     const userRepository = this.connection?.getRepository(User);
+
+    // DBに入ってるuserをuserIdで取り出す(新規の時はなんも入らん)
     const user = await userRepository?.findOne({discordId: userId});
+
     const g = await this.client.guilds.fetch(guildID);
     const general: TextChannel = <TextChannel>await g?.channels.fetch(generalChannel);
     const discordUser = await g.members.fetch(userId)
+
+    // Discordのロールを更新する ??? これおかしいかもしれん
     await discordUser.roles.remove(neruRole);
     await discordUser.roles.add(okiruRole);
+
     if (!user) {
-      // Userが未登録だった時
+      // Userが未登録だった時、ユーザを作る
       const newUser = userRepository?.create({
         discordId: userId
       });
@@ -160,6 +196,7 @@ export class NeruOkiruBot extends AppBase {
       await this.doOkiru(userId)
     }
 
+    // updateでDBに入ってる値を更新する
     await userRepository?.update(
       {discordId: userId},
       {
@@ -167,14 +204,19 @@ export class NeruOkiruBot extends AppBase {
         sleepTempTime: new Date().getTime()
       }
     );
+
+    // Discordのロールを更新
     await discordUser.roles.remove(okiruRole);
     await discordUser.roles.add(neruRole);
+
+    // generalにメッセージを送信
     await general.send(
       this.getNameFromID(userId) + "はねました。ぽやしみ"
     );
 
   }
 
+  // 睡眠データをとるメソッド
   async getUserNeruData(userId: string): Promise<{sleeps: Sleep[], nowSleeping: boolean}> {
     const userRepository = this.connection?.getRepository(User);
     const sleepRepository = this.connection?.getRepository(Sleep);
@@ -194,6 +236,7 @@ export class NeruOkiruBot extends AppBase {
     }
   }
 
+  // 起きる処理メソッド
   async doOkiru(userId: string) {
     const userRepository = this.connection?.getRepository(User);
     const sleepRepository = this.connection?.getRepository(Sleep);
@@ -201,6 +244,8 @@ export class NeruOkiruBot extends AppBase {
     const g = await this.client.guilds.fetch(guildID);
     const general: TextChannel = <TextChannel>await g?.channels.fetch(generalChannel);
     const discordUser = await g.members.fetch(userId)
+
+    // discordのロールを更新 ???
     await discordUser.roles.remove(neruRole);
     await discordUser.roles.add(okiruRole);
 
@@ -211,24 +256,35 @@ export class NeruOkiruBot extends AppBase {
       });
       await userRepository?.save(<User>newUser);
     } else if (user.nowSleeping) {
+      // ユーザが寝てる時
       const date = new Date().getTime();
+
+      // 寝たっていうデータを作成して、DBに入れてる
       const sleep = sleepRepository?.create({
         sleepTime: user.sleepTempTime,
         wakeTime: date,
         user: user
       });
       sleepRepository?.save(<Sleep>sleep);
-      // ポイントの付与
+
+
+      // ポイントの計算
       const aap = this.calcAAPoint(date - (user.sleepTempTime || 0));
+
       if (aap !== 0) {
+        // 送金
         await giveAAPoint(userId, aap);
       }
 
+      // ユーザの睡眠ログをとってくる
       const sleeps = (await sleepRepository?.find({where: {user}}))?.filter(n => (n?.wakeTime || 0) - (n?.sleepTime || 0) >= 10 * 60 * 1000);
+      // 寝てる時間をreduce(forのエモい版)で加算
       const sum = sleeps?.reduce((p, n) => {
         return ((n?.wakeTime || 0) - (n?.sleepTime || 0)) + p;
       }, 0);
 
+
+      // 画像を生成
       const uuid = await this.OGP.generateOGP({
         date: new Date(),
         icon: (discordUser.displayAvatarURL() || "https://cdn.discordapp.com/avatars/803309031560708167/f160e91772bc096338fd55962a207a52.webp"),
@@ -241,10 +297,12 @@ export class NeruOkiruBot extends AppBase {
         end: date
       });
 
+      // 画像をgeneralに送信
       await general.send(
         `https://sleep.ahoaho.jp/image/${uuid}.png`
       );
 
+      // ユーザの寝てるステータスをDBに反映
       userRepository?.update(
         {discordId: userId},
         {nowSleeping: false}
@@ -252,6 +310,7 @@ export class NeruOkiruBot extends AppBase {
     }
   }
 
+  // ボットのボタンを二つ用意するメソッド
   async initBotMessage() {
     const channel: TextChannel = (await this.client.channels.fetch(noChannel)) as TextChannel;
     const action = new ActionRowBuilder()
@@ -273,6 +332,7 @@ export class NeruOkiruBot extends AppBase {
     await channel.send({content: "このメッセージにリアクションしてね(⋈◍＞◡＜◍)。✧♡", components: [action]});
   }
 
+  // IDから名前取るメソッド
   getNameFromID(id: string) {
     let g = this.client.guilds.cache.get(guildID);
     let nickName = g?.members.cache.get(id)?.nickname?.replace("@", "＠");
@@ -280,6 +340,7 @@ export class NeruOkiruBot extends AppBase {
     return nickName;
   }
 
+  // ああポイント計算メソッド
   calcAAPoint(time: number): number {
     const hour = Math.ceil(time / (1000 * 60 * 60));
     if (0 <= hour && hour < 5) {
@@ -293,6 +354,7 @@ export class NeruOkiruBot extends AppBase {
     }
   }
 
+  // データベースつなぐ
   async connectDB() {
     this.connection = await createConnection(options);
     const userRepository = this.connection.getRepository(User);
